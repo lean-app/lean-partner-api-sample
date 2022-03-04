c932f686c1800d2f5ffa26a5a7c4ecd2fbd36065
Grant Elliott <grant@withlean.com>
2022-03-04 16:24:42 -0500

    return endpoint info to verify it's working

--- aws-sample/lib/lambda/api/workers/get.ts
+++ aws-sample/lib/lambda/api/workers/get.ts
@@ -1,1 +1,1 @@
-export const handler = async (event: any) => ({ statusCode: 200, body: JSON.stringify(event) });
+export const handler = ({ requestContext }: any) => ({ statusCode: 200, body: JSON.stringify(requestContext) });
