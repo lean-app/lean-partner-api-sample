fe2d2dfd1363fed4cbbe9d8ace5b186adfce45d6
Grant Elliott <grant@withlean.com>
2022-03-15 22:11:59 -0400

    invite worker form started

--- react-app/src/components/Worker/InviteWorkerModal.tsx
+++ react-app/src/components/Worker/InviteWorkerModal.tsx
@@ -1,3 +1,11 @@
+import Form from 'react-bootstrap/Form';
+
 export const InviteWorkerModal = ({ worker }: { worker: any}) => {
-  return <div>{JSON.stringify(worker)}</div>
+  return <Form>
+    <h1>Invite Worker</h1>
+    <Form.Group>
+      <Form.Label>First Name</Form.Label>
+      <Form.Control type="text" placeholder="First Name"></Form.Control>
+    </Form.Group>
+  </Form>
 }
