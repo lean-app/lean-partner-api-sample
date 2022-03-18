a56ee4d4e4e186329421f7ee5f02d9e687c0fbc5
Grant Elliott <grant@withlean.com>
2022-03-18 12:08:22 -0400

    async would be done with a queue lets just toss these in the db with await and get fancy later

--- aws-sample/lib/lambda/webhook/post.ts
+++ aws-sample/lib/lambda/webhook/post.ts
@@ -3,43 +3,44 @@ import { Temporal } from "@js-temporal/polyfill";
 import { APIGatewayProxyEvent, Context } from "aws-lambda";
 import { response } from "../response";
 
+const putEvent = async (event: APIGatewayProxyEvent, instant: Temporal.Instant) => {
+  try {
+    const { 
+      event: eventType, 
+      data 
+    } = JSON.parse(event.body ?? '{ }') ?? { } as { [key: string]: any };
+
+
+    const partition = instant.round('hour').epochMilliseconds.toString();
+    const attributes: { [key: string]: { 'S': string } } = { };
+    for (const key of Object.keys(data)) {
+      attributes[key] = { 'S': JSON.stringify(data[key]) };
+    }
+
+    const client = new DynamoDBClient({ });
+    const command = new PutItemCommand({
+      TableName: 'PartnerTable',
+      Item: {
+        pk: {
+          'S': `EVENT#${partition}`
+        },
+        sk: {
+          'S': `LEAN#${eventType}#${event.requestContext.requestTimeEpoch}#${event.requestContext.requestId}`
+        }, 
+        ...attributes
+      },
+    });
+
+    await client.send(command);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 export const handler = async (event: APIGatewayProxyEvent, context: Context) => {
   context.callbackWaitsForEmptyEventLoop = false;
   const instant = Temporal.Now.instant();
 
-  setTimeout(async () => {
-    try {
-      const { 
-        event: eventType, 
-        data 
-      } = JSON.parse(event.body ?? '{ }') ?? { } as { [key: string]: any };
-
-
-      const partition = instant.round('hour').epochMilliseconds.toString();
-      const attributes: { [key: string]: { 'S': string } } = { };
-      for (const key of Object.keys(data)) {
-        attributes[key] = { 'S': JSON.stringify(data[key]) };
-      }
-
-      const client = new DynamoDBClient({ });
-      const command = new PutItemCommand({
-        TableName: 'PartnerTable',
-        Item: {
-          pk: {
-            'S': `EVENT#${partition}`
-          },
-          sk: {
-            'S': `LEAN#${eventType}#${event.requestContext.requestTimeEpoch}#${event.requestContext.requestId}`
-          }, 
-          ...attributes
-        },
-      });
-
-      await client.send(command);
-    } catch (error) {
-      console.error(error);
-    }
-  }, 0);
-
-  return response(201);
+  await putEvent(event, instant);
+  return response(200);
 };
