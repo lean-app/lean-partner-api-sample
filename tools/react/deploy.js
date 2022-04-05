13906fc2223da7555e4b756ac612db6b8565a1f0
Grant Elliott <gelliott181@gmail.com>
2022-04-05 17:01:45 -0400

    improve tooling output

--- tools/react/deploy.js
+++ tools/react/deploy.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 const path = require('path');
 const fs = require('fs/promises');
 const { concat } = require('rxjs');
@@ -5,24 +7,31 @@ const { tap, map, toArray } = require('rxjs/operators');
 
 const { command } = require('../cli');
 
+const yargs = require('yargs');
+
 (async () => {
+  const { verbose = false, silent = false } = yargs.argv ?? { };
   const { AwsSampleStack: {
     ReactAppDomainName, ReactAppBucketName
   } } = JSON.parse(await fs.readFile('./aws-app/cdk-outputs.json'));
 
   concat(
     command(`aws s3 rm s3://${ReactAppBucketName} --recursive`).pipe(
+      tap((buffer) => silent || (verbose && console.log(buffer.toString()))),
       toArray(),
       map(() => 'Bucket emptied'),
     ),
     command(`aws s3 sync ${path.join(__dirname, '../../react-app/build')} s3://${ReactAppBucketName}`).pipe(
-      tap((buffer) => console.log(buffer.toString())),
+      tap((buffer) => silent || (verbose && console.log(buffer.toString()))),
       toArray(),
       map(() => 'Bucket synced'),
     ),
   ).subscribe({
-    next: (message) => console.log(message),
+    next: (message) => !silent && console.log(message),
     error: (err) => console.error(err),
-    complete: () => [ console.log('Deployment complete'), console.log(`App Url ${ReactAppDomainName}`) ],
+    complete: () => !silent && [ 
+      console.log('Deployment complete'), 
+      console.log(`App Url ${ReactAppDomainName}`) 
+    ],
   });
 })();
