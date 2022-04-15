0634b480675e0e30e1971fe14a1968453eaeb35a
Grant Elliott <gelliott181@gmail.com>
2022-04-15 15:28:47 -0400

    install react deps

--- tools/build/react.js
+++ tools/build/react.js
@@ -1,13 +1,19 @@
 #!/usr/bin/env node
 const path = require('path');
+const { switchMap } = require('rxjs');
 
 const { command } = require('../cli');
 const { verbose = false, silent = false } = require('yargs').argv;
 
-command(`npm run build`, {
+command(`npm install`, {
   cwd: path.join(__dirname, '../../react-app'),
   verbose
-}).subscribe({
+}).pipe(
+  switchMap(() => command(`npm run build`, {
+    cwd: path.join(__dirname, '../../react-app'),
+    verbose
+  }))
+).subscribe({
   error: (err) => console.error({ error: err.toString() }),
   complete: () => !silent && console.log('React build complete'),
 });
